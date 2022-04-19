a0ae3f54f4f86a42aee64c210c25660015206269
Node0 <business@node-0.com>
2022-04-18 21:51:23 -0700

    Documented usage with comments.

--- examples/mixin_scratchpad.js
+++ examples/mixin_scratchpad.js
@@ -86,25 +86,49 @@ class NanoProbe {
   }
 }
 
+// ES6 Class based OOP allows for single-inheritance only
 class Animal extends Eukaryote {
+  // All the magic happens in the constructor...🔥
   constructor(name, radius) {
+    // Pass radius to the superclass's constructor
     super(radius);
-    Mixin((this.NP = {}), new NanoProbe());
-    Mixin((this.Bact = {}), new Bacteria());
-    Mixin(this, new NanoProbe());
-    //Mixin(this.<namespace> = {}, new <ClassName>() );
     this.name = name;
+    
+    /* 
+    *  Inside the constructor of the child we can now implement multiple-inheritance.
+    *  With Polymix, we gain an idiomatic (readable) way to
+    *  achieve multiple-inheritance in a namespaces (non-colliding) manner.
+    */
+    Mixin( this.NP = {},   new NanoProbe() );
+    Mixin( this.Bact = {}, new Bacteria()  );
+    
+    /* 
+    *  If we wish to play without gaurdrails, we can Mixin to 'this' and risk overwriting
+    *  extant methods and properties on the child class.
+    */
+    Mixin( this, new NanoProbe() );
+    
+    /*
+    *  The general form of Polymix's Mixin pattern is:
+    *  Write ancestor classes and include: `makeMixable(this, <AncestorClassName>)` inside the ancestor class's constructor.
+    *  The in the descendant class (the child) include: `Mixin(this.<namespace> = {}, new <ChildClassName>() )` in the child class's constructor.
+    *  That's it, enjoy easy multiple-inheritance!
+    */
   }
 
-  // If Mixed-in directly inside the child-class's contructor i.e
-  // Mixin( this, new NanoProbe() ); and NOT Mixin( this.NP = {}, new NanoProbe() );
-  // Then there's a potential method name collision hazard, i.e. later methods of the same name
-  // will overwrite any previous methods sharing that name. By namespacing into a subobject during Mixin
-  // these collisions are avoided.
+  /* 
+  *  If Mixed-in directly inside the child-class's contructor i.e
+  *  Mixin( this, new NanoProbe() ); and NOT Mixin( this.NP = {}, new NanoProbe() );
+  *  Then there's a potential method name collision hazard, i.e. later methods of the same name
+  *  will overwrite any previous methods sharing that name. By namespacing into a subobject during Mixin
+  *  these collisions are avoided.
+  */
 
   identify() {
+    // Out of the box inherited methods from Eukaryote... plus all the other classes we pulled in 🔥
     console.log(this);
-    // Out of the box inherited methods from Eukaryote...
+    
+    // Normal access via template string interpolation.
     console.log(
       `This Animal's name is ${this.name}, its cells have a radius of ${
         this.radius
@@ -112,7 +136,7 @@ class Animal extends Eukaryote {
          ${this.cellImportinTransport("CAGTA")}, there is also ${this.foo}`
     );
     // After adding mixins...
-    if (this.NP.hasNanoProbe) {
+    if ( this.NP.hasNanoProbe ) {
       console.log(
         `Alert!!! WE ARE BORG! ${this.NP.beep()} RESISTANCE IS FUTILE, YOU WILL BE ASSIMILATED!
          But wait, there's more, our Bacterial traits include: ${this.Bact.gurgle()}`
